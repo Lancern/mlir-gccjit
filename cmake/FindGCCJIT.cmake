122acc875c6ba6078cd351d031379900af594fb5
Sirui Mu <msrlancern@gmail.com>
2024-10-27 23:09:25 +0800

    [gccjit][CMake] update CMake and support build on macOS

--- cmake/FindGCCJIT.cmake
+++ cmake/FindGCCJIT.cmake
@@ -1,15 +1,23 @@
 include_guard(GLOBAL)
 
 set(GCCJIT_DIRECTORY "" CACHE PATH "Path to GCCJIT installation")
+set(GCCJIT_INCLUDE_DIR "" CACHE PATH "Path to GCCJIT include directory")
 
 if (GCCJIT_DIRECTORY)
     message(STATUS "Using GCCJIT from ${GCCJIT_DIRECTORY}")
-    set(GCCJIT_INCLUDE_DIRS ${GCCJIT_DIRECTORY}/include)
-    set(GCCJIT_LINK_DIRS ${GCCJIT_DIRECTORY})
+
+    if (NOT GCCJIT_INCLUDE_DIR)
+        set(GCCJIT_INCLUDE_DIR ${GCCJIT_DIRECTORY}/include)
+    endif ()
+
+    set(GCCJIT_LIB_DIRS ${GCCJIT_DIRECTORY})
     if (EXISTS ${GCCJIT_DIRECTORY}/lib)
-        list(APPEND GCCJIT_LINK_DIRS ${GCCJIT_DIRECTORY}/lib)
+        list(APPEND GCCJIT_LIB_DIRS ${GCCJIT_DIRECTORY}/lib)
     endif()
-    set(CMAKE_REQUIRED_INCLUDES ${GCCJIT_INCLUDE_DIRS})
+endif ()
+
+if (GCCJIT_INCLUDE_DIR)
+    list(APPEND CMAKE_REQUIRED_INCLUDES ${GCCJIT_INCLUDE_DIR})
 endif ()
 
 include(CheckIncludeFileCXX)
@@ -18,15 +26,22 @@ if (NOT LIBGCCJIT_H_EXIST)
     message(FATAL_ERROR "could not find libgccjit.h in system headers (CMAKE_REQUIRED_INCLUDES: ${CMAKE_REQUIRED_INCLUDES})")
 endif ()
 
-add_library(libgccjit INTERFACE)
-
-if (GCCJIT_DIRECTORY)
-    find_library(GCCJIT_LIBRARY NAMES gccjit PATHS ${GCCJIT_LINK_DIRS})
+if (GCCJIT_LIB_DIRS OR GCCJIT_LIBRARY)
+    find_library(GCCJIT_LIBRARY NAMES gccjit PATHS ${GCCJIT_LIB_DIRS})
     if (NOT GCCJIT_LIBRARY)
-        message(FATAL_ERROR "could not find libgccjit in ${GCCJIT_LINK_DIRS}")
+        message(FATAL_ERROR "could not find gccjit library file at ${GCCJIT_LIB_DIRS}")
+    elseif (NOT EXISTS ${GCCJIT_LIBRARY})
+        message(FATAL_ERROR "could not find gccjit library file at ${GCCJIT_LIBRARY}")
     endif()
-    target_link_libraries(libgccjit INTERFACE ${GCCJIT_LIBRARY})
-    target_include_directories(libgccjit INTERFACE ${GCCJIT_INCLUDE_DIRS})
-else()
-    target_link_libraries(libgccjit INTERFACE gccjit)
+
+    add_library(libgccjit SHARED IMPORTED)
+    set_target_properties(libgccjit PROPERTIES
+        IMPORTED_LOCATION ${GCCJIT_LIBRARY}
+    )
+else ()
+    add_library(libgccjit INTERFACE)
+endif ()
+
+if (GCCJIT_INCLUDE_DIR)
+    target_include_directories(libgccjit INTERFACE ${GCCJIT_INCLUDE_DIR})
 endif ()
