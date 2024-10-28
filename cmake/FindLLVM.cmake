89dfa157cef9f97ecf3803206cead91a892312df
Sirui Mu <msrlancern@gmail.com>
2024-10-28 17:01:58 +0800

    [gccjit][build] update CMake to find FileCheck correctly

--- cmake/FindLLVM.cmake
+++ cmake/FindLLVM.cmake
@@ -8,22 +8,26 @@ include(${LLVM_DIR}/TableGen.cmake)
 include(${LLVM_DIR}/HandleLLVMOptions.cmake)
 
 # Try to find the FileCheck utility.
-if (NOT TARGET FileCheck)
+if (TARGET FileCheck)
+    get_target_property(LLVM_FILE_CHECK_EXE FileCheck IMPORTED_LOCATION)
+endif ()
+
+if (NOT LLVM_FILE_CHECK_EXE)
+    unset(LLVM_FILE_CHECK_EXE)
+    if (MLIR_GCCJIT_ENABLE_TEST)
+        set(filecheck_required_param REQUIRED)
+    endif ()
     find_program(LLVM_FILE_CHECK_EXE
         FileCheck
         NAMES FileCheck-${LLVM_VERSION_MAJOR}
-        HINTS "${LLVM_DIR}/../../../bin"
+        HINTS
+            "${LLVM_DIR}/../../../bin"
+            "${LLVM_DIR}/../bin"
+        ${filecheck_required_param}
     )
-    if (LLVM_FILE_CHECK_EXE)
-        add_executable(FileCheck IMPORTED)
-        set_target_properties(FileCheck PROPERTIES IMPORTED_LOCATION ${LLVM_FILE_CHECK_EXE})
-    endif ()
 endif ()
 
-if (TARGET FileCheck)
-    if (NOT LLVM_FILE_CHECK_EXE)
-        get_target_property(LLVM_FILE_CHECK_EXE FileCheck IMPORTED_LOCATION)
-    endif ()
+if (LLVM_FILE_CHECK_EXE)
     message(STATUS "Found FileCheck utility at ${LLVM_FILE_CHECK_EXE}")
 elseif (MLIR_GCCJIT_ENABLE_TEST)
     message(FATAL_ERROR "Could not find FileCheck utility")
